f08ef6dcb5252f5e3b8b033bf93824e32eae123b
lenovo <kavya@ubuntu.ubuntu-domain>
2019-03-14 21:59:23 +0530

    deploy file

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##
+##
+ENVIRONMENT=$1
+
+if [ $ENVIRONMENT = "QA" ];then
+sshpass -p "$PASSWORD" scp target/gamutkart.war kavya@172.17.0.3:/home/kavya/devops/apache-tomcat-8.5.38/webapps
+sshpass -p "$PASSWORD" ssh kavya@172.17.0.3 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+
+elif [ $ENVIRONMENT = "SIT" ];then
+sshpass -p "$PASSWORD" scp target/gamutkart.war kavya@172.17.0.4:/home/kavya/devops/apache-tomcat-8.5.38/webapps
+sshpass -p "$PASSWORD" ssh kavya@172.17.0.4 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+echo "deployment has been done!"
+fi
