eeb9e19c2e2913a4a377337e48cacfde9e3102f2
lenovo <kavya@ubuntu.ubuntu-domain>
2019-03-21 19:19:46 +0530

    permissions modify

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,18 @@
 ENVIRONMENT=$1
 
 if [ $ENVIRONMENT = "QA" ];then
-sshpass -p "kavya" scp target/gamutkart.war kavya@172.17.0.3:/home/kavya/devops/apache-tomcat-8.5.38/webapps
-sshpass -p "kavya" ssh kavya@172.17.0.3 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+sshpass -p "$PASSWORD" scp target/gamutkart.war kavya@172.17.0.3:/home/kavya/devops/apache-tomcat-8.5.38/webapps
+sshpass -p "$PASSWORD" ssh kavya@172.17.0.3 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+echo "deployment has been done!"
 
 elif [ $ENVIRONMENT = "SIT" ];then
-sshpass -p "kavya" scp target/gamutkart.war kavya@172.17.0.4:/home/kavya/devops/apache-tomcat-8.5.38/webapps
-sshpass -p "kavya" ssh kavya@172.17.0.4 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+sshpass -p "$PASSWORD" scp target/gamutkart.war kavya@172.17.0.4:/home/kavya/devops/apache-tomcat-8.5.38/webapps
+sshpass -p "$PASSWORD" ssh kavya@172.17.0.4 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
+echo "deployment has been done!"
+
+elif [ $ENVIRONMENT = "PROD" ];then
+sshpass -p "$PASSWORD" scp target/gamutkart.war kavya@172.17.0.5:/home/kavya/devops/apache-tomcat-8.5.38/webapps
+sshpass -p "$PASSWORD" ssh kavya@172.17.0.5 "JAVA_HOME=/home/kavya/devops/jdk1.8.0_201/" /home/kavya/devops/apache-tomcat-8.5.38/bin/startup.sh
 echo "deployment has been done!"
 
 fi
